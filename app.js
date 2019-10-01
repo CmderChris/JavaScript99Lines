6f24270408f054b3f4dd73a8bdb9e5a9da1ccd3c
Chris Smith <christopherkeithsmith@gmail.com>
2019-10-01 11:56:51 -0500

    working!

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ for (let i = 0; i < friends.length; i++) {
     for (let j = 99; j > 0; j--) {
         if (j === 1) { 
             //custom lyrics for the last line
-            console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file.'); 
+            console.log(j + ' line of code in the file, ' + j + ' line of code; ' + friends[i] + ' strikes it out, clears it all out, no more lines of code in the file!'); 
         } else {
             //default lyrics for values of j 99 to 2
             console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file.'); 
