b444e8487f08ef113800605aea1836590d1f6790
Chris Smith <christopherkeithsmith@gmail.com>
2019-10-01 11:53:06 -0500

    fixed syntax

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-let friend = ['Emily', 'Luke', 'Audrey', 'Julian', 'Oliver'];
+let friends = ['Emily', 'Luke', 'Audrey', 'Julian', 'Oliver'];
 
-for (let i = 0; i < friend.length; i++) {
+for (let i = 0; i < friends.length; i++) {
+   
+    //display each friends name
+    console.log(friends[i] + ':')
+
     for (let j = 99; j > 0; j--) {
-        if (j > 2)
-            console.log(+ j + ' lines of code in the file, ' + j + ' lines of code; ' + friend + ' strikes one out, clears it all out, ' + (j--) + ' lines of code in the file.');
-    }}
+        if (j === 1) { 
+            //custom lyrics for the last line
+            console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file.'); 
+        } else {
+            //default lyrics for values of j 99 to 2
+            console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file.'); 
+        }
+    }
+
+    console.log('------------------------------------');
+}
